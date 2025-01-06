5ca00f2f4afffd9c131660cdcfd116fba54bdf1e
Ismat9999 <redgoo123@gmail.com>
2025-01-06 16:14:40 +0500

    Gemini Ai GetX

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
+
+
 import '../controller/home_controller.dart';
 import '../widgets/item_gemini_message.dart';
 import '../widgets/item_user_message.dart';
@@ -14,11 +16,9 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-String res= "no data";
-
 class _HomePageState extends State<HomePage> {
+  var _controller = Get.find<HomeController>();
 
-  var _controller =Get.find<HomeController>();
   @override
   void initState() {
     super.initState();
@@ -27,9 +27,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-      body:GetBuilder<HomeController>(
-        builder: (_){
+      backgroundColor: Colors.black,
+      body: GetBuilder<HomeController>(
+        builder: (_) {
           return Container(
             padding: EdgeInsets.only(bottom: 20, top: 10),
             child: Column(
@@ -37,76 +37,70 @@ class _HomePageState extends State<HomePage> {
                 const SizedBox(
                   height: 45,
                   child: Image(
-                      image:AssetImage("assets/images/gemini_logo.png"),
-                  fit: BoxFit.cover,),
+                    image: AssetImage('assets/images/gemini_logo.png'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 Expanded(
-                  child: Stack(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(15),
-                        child: _controller.messages.isNotEmpty
-                          ? ListView.builder(
-                          controller: _controller.scrollController,
+                    child: Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(15),
+                          child: _controller.messages.isNotEmpty
+                              ? ListView.builder(
+                            controller: _controller.scrollController,
                             itemCount: _controller.messages.length,
-                          itemBuilder: (context, index){
-                            var message=_controller.messages[index];
-                            if(message.isMine!){
-                              return itemOfUserMessage(message);
-                            }else{
-                              return itemOfGeminiMessage(message);
-                            }
-                          },
-                        )
-                            : Center(
-                          child: SizedBox(
-                            height: 80,
-                            width: 80,
-                            child:   Image.asset
-                              ("assets/images/gemini_icon.png"),
+                            itemBuilder: (context, index) {
+                              var message = _controller.messages[index];
+                              if (message.isMine!) {
+                                return itemOfUserMessage(message);
+                              } else {
+                                return itemOfGeminiMessage(message);
+                              }
+                            },
+                          )
+                              : Center(
+                            child: SizedBox(
+                              height: 80,
+                              width: 80,
+                              child: Image.asset('assets/images/gemini_icon.png'),
                             ),
                           ),
                         ),
-                        _controller.isLoading
-              ? Center(
-                          child:Container(
-                            width: 40,
-                            height: 40,
-                            child: Lottie.asset("assets/lotties/lottie.json"),
-                          ),
-                        ): SizedBox.shrink(),
-                    ],
-                  ),
-                ),
+                        _controller.isLoading ? Center(child: Container(
+                          width: 40,
+                          height: 40,
+                          child: Lottie.asset("assets/lotties/lottie1.json"),
+                        ),) : SizedBox.shrink(),
+                      ],)),
                 Container(
                   margin: const EdgeInsets.only(right: 20, left: 20),
                   padding: const EdgeInsets.only(left: 20),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(25),
-                    border: Border.all(color: Colors.grey,width: 1.5),
+                    border: Border.all(color: Colors.grey, width: 1.5),
                   ),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      _controller.pickedImage64.isNotEmpty
-                      ? Stack(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.all(10),
-                            width: 70,
-                            height: 70,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(color: Colors.white),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.memory(
-                                  base64Decode(_controller.pickedImage64),
-                              fit: BoxFit.cover,),
+                    children: [_controller.pickedImage64.isNotEmpty ? Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          width: 70,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Colors.white),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.memory(
+                              base64Decode(_controller.pickedImage64),
+                              fit: BoxFit.cover,
                             ),
                           ),
-                          Container(
+                        ),
+                        Container(
                             margin: EdgeInsets.all(10),
                             width: 70,
                             height: 70,
@@ -116,15 +110,18 @@ class _HomePageState extends State<HomePage> {
                             ),
                             child: Center(
                               child: IconButton(
-                                onPressed: (){
+                                onPressed: () {
                                   _controller.removePickedImage();
                                 },
-                                icon: Icon(Icons.clear,color: Colors.black,),
+                                icon: Icon(
+                                  Icons.clear,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                          ),
-                        ],
-                      ): SizedBox.shrink(),
+                            )),
+                      ],
+                    )
+                        : SizedBox.shrink(),
                       Row(
                         children: [
                           Expanded(
@@ -140,8 +137,8 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ),
                           IconButton(
-                            onPressed: (){
-                              _controller.pickedImageFromGallery();
+                            onPressed: () {
+                               _controller.pickImageFromGallery();
                             },
                             icon: Icon(
                               Icons.attach_file,
@@ -149,7 +146,7 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ),
                           IconButton(
-                            onPressed: (){
+                            onPressed: () {
                               _controller.askToGemini();
                             },
                             icon: Icon(
